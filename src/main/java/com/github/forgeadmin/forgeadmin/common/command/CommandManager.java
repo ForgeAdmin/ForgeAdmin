e4e719d7cf9dc5e4c5b1bdec7c2102936ebe07a7
hobos-taco <hobos_taco@hotmail.com>
2016-05-03 22:26:16 +1000

    added remainder of commands

--- src/main/java/com/github/forgeadmin/forgeadmin/common/command/CommandManager.java
+++ src/main/java/com/github/forgeadmin/forgeadmin/common/command/CommandManager.java
@@ -1,8 +1,15 @@
 package com.github.forgeadmin.forgeadmin.common.command;
 
+import com.github.forgeadmin.forgeadmin.api.permissions.Permissible;
 import com.github.forgeadmin.forgeadmin.common.command.overwrite.*;
+import com.github.forgeadmin.forgeadmin.common.command.overwrite.server.*;
 import net.minecraft.command.*;
-import net.minecraft.command.server.CommandMessage;
+import net.minecraft.command.server.*;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingFallEvent;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 import java.util.Map;
@@ -20,32 +27,63 @@ public class CommandManager {
     replaceCommand(new CommandDifficulty(), new CommandDifficultyOverwrite());
     replaceCommand(new CommandDefaultGameMode(), new CommandDefaultGameModeOverwrite());
     replaceCommand(new CommandKill(), new CommandKillOverwrite());
-    replaceCommand(new CommandToggleDownfall(), new CommandXPOverwrite());
+    replaceCommand(new CommandToggleDownfall(), new CommandToggleDownfallOverwrite());
     replaceCommand(new CommandWeather(), new CommandWeatherOverwrite());
+    replaceCommand(new CommandXP(), new CommandXPOverwrite());
+    replaceCommand(new CommandTeleport(), new CommandTeleportOverwrite());
     replaceCommand(new CommandGive(), new CommandGiveOverwrite());
     replaceCommand(new CommandReplaceItem(), new CommandReplaceItemOverwrite());
     replaceCommand(new CommandStats(), new CommandStatsOverwrite());
     replaceCommand(new CommandEffect(), new CommandEffectOverwrite());
     replaceCommand(new CommandEnchant(), new CommandEnchantOverwrite());
     replaceCommand(new CommandParticle(), new CommandParticleOverwrite());
+    replaceCommand(new CommandEmote(), new CommandEmoteOverwrite());
     replaceCommand(new CommandShowSeed(), new CommandShowSeedOverwrite());
     replaceCommand(new CommandHelp(), new CommandHelpOverwrite());
     replaceCommand(new CommandDebug(), new CommandDebugOverwrite());
+    replaceCommand(new CommandMessage(), new CommandMessageOverwrite());
+    replaceCommand(new CommandBroadcast(), new CommandBroadcastOverwrite());
     replaceCommand(new CommandSetSpawnpoint(), new CommandSetSpawnpointOverwrite());
+    replaceCommand(new CommandSetDefaultSpawnpoint(), new CommandSetDefaultSpawnpointOverwrite());
     replaceCommand(new CommandGameRule(), new CommandGameRuleOverwrite());
     replaceCommand(new CommandClearInventory(), new CommandClearInventoryOverwrite());
+    replaceCommand(new CommandTestFor(), new CommandTestForOverwrite());
     replaceCommand(new CommandSpreadPlayers(), new CommandSpreadPlayersOverwrite());
     replaceCommand(new CommandPlaySound(), new CommandPlaySoundOverwrite());
+    replaceCommand(new CommandScoreboard(), new CommandScoreboardOverwrite());
     replaceCommand(new CommandExecuteAt(), new CommandExecuteAtOverwrite());
     replaceCommand(new CommandTrigger(), new CommandTriggerOverwrite());
+    replaceCommand(new CommandAchievement(), new CommandAchievementOverwrite());
+    replaceCommand(new CommandSummon(), new CommandSummonOverwrite());
+    replaceCommand(new CommandSetBlock(), new CommandSetBlockOverwrite());
     replaceCommand(new CommandFill(), new CommandFillOverwrite());
     replaceCommand(new CommandClone(), new CommandCloneOverwrite());
     replaceCommand(new CommandCompare(), new CommandCompareOverwrite());
     replaceCommand(new CommandBlockData(), new CommandBlockDataOverwrite());
+    replaceCommand(new CommandTestForBlock(), new CommandTestForBlockOverwrite());
+    replaceCommand(new CommandMessageRaw(), new CommandMessageRawOverwrite());
     replaceCommand(new CommandWorldBorder(), new CommandWorldBorderOverwrite());
     replaceCommand(new CommandTitle(), new CommandTitleOverwrite());
-    replaceCommand(new CommandToggleDownfall(), new CommandToggleDownfallOverwrite());
-    replaceCommand(new CommandXP(), new CommandXPOverwrite());
+    replaceCommand(new CommandEntityData(), new CommandEntityDataOverwrite());
+    if (MinecraftServer.getServer().isDedicatedServer()) {
+      replaceCommand(new CommandOp(), new CommandOpOverwrite());
+      replaceCommand(new CommandDeOp(), new CommandDeOpOverwrite());
+      replaceCommand(new CommandStop(), new CommandStopOverwrite());
+      replaceCommand(new CommandSaveAll(), new CommandSaveAllOverwrite());
+      replaceCommand(new CommandSaveOff(), new CommandSaveOffOverwrite());
+      replaceCommand(new CommandSaveOn(), new CommandSaveOnOverwrite());
+      replaceCommand(new CommandBanIp(), new CommandBanIpOverwrite());
+      replaceCommand(new CommandPardonIp(), new CommandPardonIpOverwrite());
+      replaceCommand(new CommandBanPlayer(), new CommandBanPlayerOverwrite());
+      replaceCommand(new CommandListBans(), new CommandListBansOverwrite());
+      replaceCommand(new CommandPardonPlayer(), new CommandPardonPlayerOverwrite());
+      replaceCommand(new CommandServerKick(), new CommandServerKickOverwrite());
+      replaceCommand(new CommandListPlayers(), new CommandListPlayersOverwrite());
+      replaceCommand(new CommandWhitelist(), new CommandWhitelistOverwrite());
+      replaceCommand(new CommandSetPlayerTimeout(), new CommandSetPlayerTimeoutOverwrite());
+    } else {
+      replaceCommand(new CommandPublishLocalServer(), new CommandPublishLocalServerOverwrite());
+    }
   }
 
   private static void replaceCommand(Class<CommandMessage> clazz, ICommand newCommand) {
---
+++ src/main/java/com/github/forgeadmin/forgeadmin/common/command/overwrite/server/CommandPardonPlayerOverwrite.java
@@ -0,0 +1,25 @@
+package com.github.forgeadmin.forgeadmin.common.command.overwrite.server;
+
+import com.github.forgeadmin.forgeadmin.api.permissions.Permissible;
+import com.github.forgeadmin.forgeadmin.api.permissions.PermissonHandler;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.command.server.CommandAchievement;
+import net.minecraft.command.server.CommandPardonPlayer;
+import net.minecraft.entity.player.EntityPlayer;
+
+public class CommandPardonPlayerOverwrite extends CommandPardonPlayer implements Permissible {
+  @Override
+  public int getRequiredPermissionLevel() {
+    return 0;
+  }
+
+  @Override
+  public String getPermissionNode() {
+    return "minecraft.command." + getCommandName();
+  }
+
+  @Override
+  public boolean canCommandSenderUseCommand(ICommandSender sender) {
+    return !(sender instanceof EntityPlayer) || PermissonHandler.getPermissionHandler().hasPermission ((EntityPlayer) sender, this);
+  }
+}
